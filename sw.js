75c9d4cb5454df8b9036cee69b66b95068ed7713
tangyi1219 <970472930@qq.com>
2026-08-02 02:54:51 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'grit-v1';
+const URLS = ['/','/grit.html','/download.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
