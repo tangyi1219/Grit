4e51efe56f22a40f19bf4e0a27126cd3f1c1738b
tangyi1219 <970472930@qq.com>
2026-08-02 04:33:10 +0800

    Add files via upload
    修复 Bug + 优化

--- sw.js
+++ sw.js
@@ -1,18 +1,20 @@
-const CACHE = 'grit-v1';
-const URLS = ['/','/grit.html','/download.html'];
+const CACHE = 'grit-v2';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+// 激活时清理旧缓存
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
 
+// 网络优先策略：在线时始终获取最新版本，离线时使用缓存
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       if (res.ok) {
         const clone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
       }
       return res;
-    }))
+    }).catch(() => caches.match(e.request))
   );
 });
